b31ef1ce19fa201c30542a9008f7529c1591ac19
thescottbob <scottmcmains@gmail.com>
2018-08-23 17:30:30 -0700

    ScottsJShw3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,6 @@
-<script>
-    The Psychic Game
-    Guess what letter I'm thinking of
-    Wins:
-    Losses:
-    Guesses Left:
-    Your Guesses So Far:
-</script>
+// When user loads page, all HTML appears and computer chooses a random letter
+// If user types a letter, then display the letter in the 'Your Guesses So Far' row
+// If letterGuessed is the same as letterGenerated, then Wins counter increases by 1
+// If letterGuessed is different from the letterGenerated, then 
+// When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
+// When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
