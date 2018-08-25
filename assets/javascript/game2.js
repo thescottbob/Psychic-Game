f0513e86de20800eed2f0caeea8ba40a78c3af36
thescottbob <scottmcmains@gmail.com>
2018-08-24 20:36:37 -0700

    ScottsJShw3

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,21 +1,26 @@
-//Mix of coding & pseudocoding outline:
+//Pseudocoding outline below
+
+// If letterGuessed is the same as letterGenerated, then Wins counter increases by 1
+
+// If letterGuessed is different from letterGenerated && if guessCount=9, then Losses counter increases by 1 && guessCount goes to zero
 
 // When user loads page, all HTML appears && the computer chooses a random letter. Here are the computer's options:
 var computerOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// The initial values for each round are set here
+// The initial values for each round
 let wins = 0;
 let losses = 0;
-let guessesRemaining = 9;
 let guesses = 9;
+let guessesRemaining = 9;
 let guessedLetters = [];
 var letterToGuess = null;
 
-// The computer will choose a random letter ('computerGuess') from the 'computerOptions' array
+//The computer will choose a random letter from the 'computerOptions' array
 var computerGuess = computerOptions[Math.floor(Math.random() * computerOptions.length)];
 
-// User will begin each round with 9 guesses. Below we select the HTML 'Guesses Remaining' element by its ID and set its value equal to 'guessesRemaining'
+// User will begin each round with 9 guesses
 function updateguessesRemaining() {
+    // Here we are grabbing the HTML element and setting it equal to the guessesRemaining. (i.e. guessesRemaining will get displayed in HTML)
     document.querySelector('#guessesRemaining').innerHTML = "Guesses Remaining: " + guessesRemaining;
 };
 
@@ -24,11 +29,11 @@ function updateLetterToGuess() {
 };
 
 function updateGuesses() {
-    // If user types a letter, then that letter will be rendered to the page
+    // If user types a letter, then display the letter in the 'Your Guesses So Far' row
     document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
 };
 
-// The below function will reset the game by calling the three new functions defined above
+// This function will reset the game
 var reset = function() {
     totalGuesses = 9;
     guessesRemaining = 9;
@@ -79,5 +84,3 @@ document.onkeyup = function(event) {
     }
 
 };
-
-
